f35784fc68f4bdde3dc08a31b76753e79d5c3cdc
jameswells <jameswells@pingidentity.com>
2021-02-09 10:33:19 -0600

    updated messages printed during config/revert

--- .gitlab-ci/00-configure-solution.sh
+++ .gitlab-ci/00-configure-solution.sh
@@ -32,6 +32,8 @@ echo "Performing base PingOne configuration"
 echo "Performing PingOne configuration revertion"
 echo "Environment ID is $ENV_ID"
 echo "API URL path is $API_LOCATIONS"
+echo "Current pipeline version is $CURRENT_PIPELINE_VERSION"
+echo "Current Org ID is $ORG_ID"
 
 #performing initial PingOne creation scripts
 for script in ./Solutions/WF/$CURRENT_PIPELINE_VERSION/PingOne/*create.sh; do
--- .gitlab-ci/01-revert-solution.sh
+++ .gitlab-ci/01-revert-solution.sh
@@ -3,6 +3,9 @@
 
 echo "Performing PingOne configuration revertion"
 echo "Environment ID is $ENV_ID"
+echo "API URL path is $API_LOCATIONS"
+echo "Current pipeline version is $CURRENT_PIPELINE_VERSION"
+echo "Current Org ID is $ORG_ID"
 
 
 #performing final PingOne revertion/deletion scripts
