92859b54f8cf33c7d888483ababa6017fa28d45d
jameswells <jameswells@pingidentity.com>
2021-02-09 11:19:21 -0600

    fix token gen

--- .gitlab-ci/00-configure-solution.sh
+++ .gitlab-ci/00-configure-solution.sh
@@ -29,6 +29,12 @@ PING_IDENTITY_DEVOPS_TAG=${PING_IDENTITY_DEVOPS_TAG:-latest}
 DEVOPS
 
 
+export WORKER_APP_ACCESS_TOKEN=$(curl -u $CLIENT_ID:$CLIENT_SECRET \
+--location --request POST "https://auth.pingone.com/$ENV_ID/as/token" \
+--header "Content-Type: application/x-www-form-urlencoded" \
+--data-raw 'grant_type=client_credentials' \
+| jq -r '.access_token')
+
 echo "Worker token is $WORKER_APP_ACCESS_TOKEN"
 
 echo "Performing base PingOne configuration"
