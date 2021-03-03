d462fa1ddd9bc42c194876211ba7c3079d10b7b9
wesleymccollam <wesleymccollam@pingidentity.com>
2021-03-03 09:22:33 -0600

    Added robust check for 600-sample_app_creation_set.sh - SOL-121

--- Solutions/CIAM/PingOne/600-sample_app_creation_set.sh
+++ Solutions/CIAM/PingOne/600-sample_app_creation_set.sh
@@ -2,138 +2,191 @@
 
 # configure PingOne Sample SAML Apps for CIAM
 #Variables needed to be passed for this script:
-#API_LOCATION
-#ENV_ID
-#WORKER_APP_ACCESS_TOKEN
+# API_LOCATION
+# ENV_ID
+# WORKER_APP_ACCESS_TOKEN
 
+# set global api call retry limit - this can be set to desired amount, default is 2
+api_call_retry_limit=2
+
+# get signing key to assign to all applications
 SIGNING_KEY_ID=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/keys" \
 --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" | jq -rc '._embedded.keys[] | select(.usageType=="SIGNING") | .id')
 
 # create Demo App - Self-Service Registration
-CREATE_SAMPLE_APP1=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
---data-raw '{
-    "name": "Demo App - Self-Service Registration",
-    "description": "This is a sample app used to demonstrate a Self-Service Registration policy with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
-    "enabled": true,
-    "type": "WEB_APP",
-    "protocol": "SAML",
-    "spEntityId": "demo_app_1",
-    "responseSigned": false,
-    "sloBinding": "HTTP_POST",
-    "acsUrls": [
-    "https://httpbin.org/anything"
-    ],
-    "assertionDuration": 60,
-    "assertionSigned": true,
-    "idpSigning": {
-        "key": {
-            "id": "'"$SIGNING_KEY_ID"'"
-        },
-        "algorithm": "SHA256withRSA"
-    }
-}')
+ssr_app_try=0
+function create_ssr_app() {
+    CREATE_SSR_APP=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
+    --data-raw '{
+        "name": "Demo App - Self-Service Registration",
+        "description": "This is a sample app used to demonstrate a Self-Service Registration policy with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
+        "enabled": true,
+        "type": "WEB_APP",
+        "protocol": "SAML",
+        "spEntityId": "demo_app_1",
+        "responseSigned": false,
+        "sloBinding": "HTTP_POST",
+        "acsUrls": [
+        "https://httpbin.org/anything"
+        ],
+        "assertionDuration": 60,
+        "assertionSigned": true,
+        "idpSigning": {
+            "key": {
+                "id": "'"$SIGNING_KEY_ID"'"
+            },
+            "algorithm": "SHA256withRSA"
+        }
+    }')
+}
 
-# checks app created, as well as verify expected app name to ensure creation
-CREATE_SAMPLE_APP1_RESULT=$(echo $CREATE_SAMPLE_APP1 | sed 's@.*}@@')
-if [ $CREATE_SAMPLE_APP1_RESULT == "201" ] ; then
+# call create self-service registration app function above
+create_ssr_app
 
-    CHECK_SAMPLE_APP1_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
+# verify application content to verify app creation
+function check_ssr_app_content() {
+    CHECK_SSR_APP_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
     --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" | jq -rc '._embedded.applications[] | select(.name=="Demo App - Self-Service Registration") | .name')
 
-    if [ "$CHECK_SAMPLE_APP1_CONTENT" == "Demo App - Self-Service Registration" ]; then
-        echo "Demo App - Self-Service Registration app added and verified content..."
+    if [ "$CHECK_SSR_APP_CONTENT" == "Demo App - Self-Service Registration" ]; then
+        echo "Self-Service Registration app exists and verified content..."
     else
-        echo "Demo App - Self-Service Registration app added, however unable to verified content!"
+        echo "Unable to locate Self-Service Registration app, or application does not exist!"
     fi
+}
+
+# checks app created, as well as verify expected app name to ensure creation
+CREATE_SSR_APP_RESULT=$(echo $CREATE_SSR_APP | sed 's@.*}@@')
+if [ $CREATE_SSR_APP_RESULT == "201" ]; then
+    echo "Self-Service Registration demo app added, beginning content check..."
+    check_ssr_app_content
+elif [[ $CREATE_SSR_APP_RESULT != "201" ]] && [[ "$ssr_app_try" < "$api_call_retry_limit" ]]; then
+    ssr_app_try=$((ssr_app_try+1))
+    echo "Self-Service Registration demo app NOT added! Retrying one more time in case of latency, as well as checking app existence..."
+    check_ssr_app_content
+    create_ssr_app
 else
-    echo "Demo App - Self-Service Registration app NOT added!"
+    echo "Self-Service Registration demo app does NOT exist and/or attempts to create exceeded!"
     exit 1
 fi
 
+
 # create Demo App - Passwordless Login SMS Only
-CREATE_SAMPLE_APP2=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
---data-raw '{
-    "name": "Demo App - Passwordless Login SMS Only",
-    "description": "This is a sample app used to demonstrate a Passwordless Login SMS Only policy with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
-    "enabled": true,
-    "type": "WEB_APP",
-    "protocol": "SAML",
-    "spEntityId": "demo_app_2",
-    "responseSigned": false,
-    "sloBinding": "HTTP_POST",
-    "acsUrls": [
-    "https://httpbin.org/anything"
-    ],
-    "assertionDuration": 60,
-    "assertionSigned": true,
-    "idpSigning": {
-        "key": {
-            "id": "'"$SIGNING_KEY_ID"'"
-        },
-        "algorithm": "SHA256withRSA"
-    }
-}')
+sms_app_try=0
+function create_sms_app() {
+    CREATE_SMS_APP=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
+    --data-raw '{
+        "name": "Demo App - Passwordless Login SMS Only",
+        "description": "This is a sample app used to demonstrate a Passwordless Login SMS Only policy with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
+        "enabled": true,
+        "type": "WEB_APP",
+        "protocol": "SAML",
+        "spEntityId": "demo_app_2",
+        "responseSigned": false,
+        "sloBinding": "HTTP_POST",
+        "acsUrls": [
+        "https://httpbin.org/anything"
+        ],
+        "assertionDuration": 60,
+        "assertionSigned": true,
+        "idpSigning": {
+            "key": {
+                "id": "'"$SIGNING_KEY_ID"'"
+            },
+            "algorithm": "SHA256withRSA"
+        }
+    }')
+}
 
-# checks app created, as well as verify expected app name to ensure creation
-CREATE_SAMPLE_APP2_RESULT=$(echo $CREATE_SAMPLE_APP2 | sed 's@.*}@@')
-if [ $CREATE_SAMPLE_APP2_RESULT == "201" ] ; then
+# call create sms passwordless app function above
+create_sms_app
 
-    CHECK_SAMPLE_APP2_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
+# verify application content to verify app creation
+function check_sms_app_content() {
+    CHECK_SMS_APP_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
     --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" | jq -rc '._embedded.applications[] | select(.name=="Demo App - Passwordless Login SMS Only") | .name')
 
-    if [ "$CHECK_SAMPLE_APP2_CONTENT" == "Demo App - Passwordless Login SMS Only" ]; then
-        echo "Passwordless Login SMS Only app added and verified content..."
+    if [ "$CHECK_SMS_APP_CONTENT" == "Demo App - Passwordless Login SMS Only" ]; then
+        echo "Passwordless Login SMS Only app exists and verified content..."
     else
-        echo "Passwordless Login SMS Only app added, however unable to verified content!"
+        echo "Passwordless Login SMS Only app exists, however unable to verified content!"
     fi
+}
+
+# checks app created, as well as verify expected app name to ensure creation
+CREATE_SMS_APP_RESULT=$(echo $CREATE_SMS_APP | sed 's@.*}@@')
+if [ $CREATE_SMS_APP_RESULT == "201" ] ; then
+    echo "Passwordless Login SMS Only app added, beginning content check..."
+    check_sms_app_content
+elif [[ $CREATE_SMS_APP_RESULT != "201" ]] && [[ "$sms_app_try" < "$api_call_retry_limit" ]]; then
+    sms_app_try=$((sms_app_try+1))
+    echo "Passwordless Login SMS Only app NOT added! Retrying one more time in case of latency, as well as checking app existence..."
+    check_sms_app_content
+    create_sms_app
 else
-    echo "Passwordless Login SMS Only app NOT added!"
+    echo "Passwordless Login SMS Only app does NOT exist and/or attempts to create exceeded!"
     exit 1
 fi
 
 # create Demo App - Passwordless Login Any Method
-CREATE_SAMPLE_APP3=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
---header 'Content-Type: application/json' \
---header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
---data-raw '{
-    "name": "Demo App - Passwordless Login Any Method",
-    "description": "This is a sample app used to demonstrate a Passwordless Login Any Method with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
-    "enabled": true,
-    "type": "WEB_APP",
-    "protocol": "SAML",
-    "spEntityId": "demo_app_3",
-    "responseSigned": false,
-    "sloBinding": "HTTP_POST",
-    "acsUrls": [
-    "https://httpbin.org/anything"
-    ],
-    "assertionDuration": 60,
-    "assertionSigned": true,
-    "idpSigning": {
-        "key": {
-            "id": "'"$SIGNING_KEY_ID"'"
-        },
-        "algorithm": "SHA256withRSA"
-    }
-}')
+pwdless_app_try=0
+function create_pwdless_app() {
+    CREATE_PWDLESS_APP=$(curl -s --write-out "%{http_code}\n" --location --request POST "$API_LOCATION/environments/$ENV_ID/applications" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" \
+    --data-raw '{
+        "name": "Demo App - Passwordless Login Any Method",
+        "description": "This is a sample app used to demonstrate a Passwordless Login Any Method with an Idp-Initiated SAML connection to a public SP API-Based application. You can use the PingIdentity SAML Decoder tool to copy and paste the SAMLResponse from the httpbin value. https://developer.pingidentity.com/en/tools/saml-decoder.html",
+        "enabled": true,
+        "type": "WEB_APP",
+        "protocol": "SAML",
+        "spEntityId": "demo_app_3",
+        "responseSigned": false,
+        "sloBinding": "HTTP_POST",
+        "acsUrls": [
+        "https://httpbin.org/anything"
+        ],
+        "assertionDuration": 60,
+        "assertionSigned": true,
+        "idpSigning": {
+            "key": {
+                "id": "'"$SIGNING_KEY_ID"'"
+            },
+            "algorithm": "SHA256withRSA"
+        }
+    }')
+}
 
-# checks app created, as well as verify expected app name to ensure creation
-CREATE_SAMPLE_APP3_RESULT=$(echo $CREATE_SAMPLE_APP3 | sed 's@.*}@@')
-if [ $CREATE_SAMPLE_APP3_RESULT == "201" ] ; then
+# call create passwordless app function above
+create_pwdless_app
 
-    CHECK_SAMPLE_APP3_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
+# verify application content to verify app creation
+function check_pwdless_app_content() {
+    CHECK_PWDLESS_APP_CONTENT=$(curl -s --location --request GET "$API_LOCATION/environments/$ENV_ID/applications" \
     --header "Authorization: Bearer $WORKER_APP_ACCESS_TOKEN" | jq -rc '._embedded.applications[] | select(.name=="Demo App - Passwordless Login Any Method") | .name')
 
-    if [ "$CHECK_SAMPLE_APP3_CONTENT" == "Demo App - Passwordless Login Any Method" ]; then
-        echo "Demo App - Passwordless Login Any Method app added and verified content..."
+    if [ "$CHECK_PWDLESS_APP_CONTENT" == "Demo App - Passwordless Login Any Method" ]; then
+        echo "Passwordless Login Any Method app exists and verified content..."
     else
-        echo "Demo App - Passwordless Login Any Method app added, however unable to verified content!"
+        echo "Passwordless Login Any Method app exists, however unable to verified content!"
     fi
+}
+
+# checks app created, as well as verify expected app name to ensure creation
+CREATE_PWDLESS_APP_RESULT=$(echo $CREATE_PWDLESS_APP | sed 's@.*}@@')
+if [ $CREATE_PWDLESS_APP_RESULT == "201" ] ; then
+    echo "Passwordless Login Any Method app added, beginning content check..."
+    check_pwdless_app_content
+elif [[ $CREATE_PWDLESS_APP_RESULT != "201" ]] && [[ "$pwdless_app_try" < "$api_call_retry_limit" ]]; then
+    pwdless_app_try=$((pwdless_app_try+1))
+    echo "Passwordless Login Any Method app NOT added! Retrying one more time in case of latency, as well as checking app existence..."
+    check_pwdless_app_content
+    create_pwdless_app
 else
-    echo "Demo App - Passwordless Login Any Method NOT added!"
+    echo "Passwordless Login Any Method app does NOT exist and/or attempts to create exceeded!"
     exit 1
 fi
