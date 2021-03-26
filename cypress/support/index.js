a4d987d4825ecd1f66a93cafbb2e45a6fe91e430
wesleymccollam <wesleymccollam@pingidentity.com>
2021-03-26 15:31:59 -0500

    Removing everything besides server-profile

--- cypress/support/index.js
+++
@@ -1,8 +0,0 @@
-afterEach(() => {
-	cy.window().then(win => {
-		if (typeof win.gc === 'function') {
-			// calling this more often seems to trigger major GC event more reliably
-			win.gc(); win.gc(); win.gc(); win.gc(); win.gc();
-		}
-	});
-});
