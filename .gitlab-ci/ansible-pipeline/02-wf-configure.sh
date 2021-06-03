f7f220d55e889854c0868a426bf3084f86229988
James Wells <jameswells@pingidentity.com>
2021-06-03 15:02:54 +0000

    updated 02 scripts in pipeline to do very very verbose

--- .gitlab-ci/ansible-pipeline/02-wf-configure.sh
+++ .gitlab-ci/ansible-pipeline/02-wf-configure.sh
@@ -2,5 +2,6 @@
 
 export ENV_ID="$WF_ENV_ID"
 export PINGFED_BASE_URL="$WF_PINGFED_BASE_URL"
-export PLAYBOOK='pf_post_playbook.yml'
+# -vvv in below is to trick very very verbose without modifying image.
+export PLAYBOOK='pf_post_playbook.yml -vvv'
 sh /ansible/entrypoint.sh
