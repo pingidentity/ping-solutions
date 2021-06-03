aae41fb9c3434b524db1874e94485e3e338fdd66
James Wells <jameswells@pingidentity.com>
2021-06-03 15:06:21 +0000

    update 01 to be vvv during pipeline without modifying image.

--- .gitlab-ci/ansible-pipeline/01-ciam-configure.sh
+++ .gitlab-ci/ansible-pipeline/01-ciam-configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export ENV_ID="$CIAM_ENV_ID"
-export PLAYBOOK='p1_pf_playbook.yml'
+# -vvv in below is to trick very very verbose without modifying image.
+export PLAYBOOK='p1_pf_playbook.yml -vvv'
 sh /ansible/entrypoint.sh
--- .gitlab-ci/ansible-pipeline/01-wf-configure.sh
+++ .gitlab-ci/ansible-pipeline/01-wf-configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export ENV_ID="$WF_ENV_ID"
-export PLAYBOOK='p1_pf_playbook.yml'
+# -vvv in below is to trick very very verbose without modifying image.
+export PLAYBOOK='p1_pf_playbook.yml -vvv'
 sh /ansible/entrypoint.sh
